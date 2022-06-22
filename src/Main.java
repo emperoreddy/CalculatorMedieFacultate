3f087b5e1ba7d02c25ff794b899a2344f481e344
Amortitu Bogdan <emperoreddy@gmail.com>
2022-06-22 23:41:18 +0300

    New algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,48 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
-        int[][] matrice1 = new int[10][1];
-        int[][] matrice2 = new int[10][1];
+        ArrayList<Integer> matrice1 = new ArrayList<>();
+        ArrayList<Integer> matrice2 = new ArrayList<>();
+        int i = 0;
         int sum = 0;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("PUNE 0 CAND TERMINI DE CITIT DATELE");
-        for (int i = 0; i < 15; i++) {
+        System.out.println("PUNE O LITERA CAND TERMINI DE CITIT DATELE");
+        boolean stopLoop = false;
+        while (!stopLoop) {
+            System.out.println("---------");
             System.out.println("Materia " + (i + 1));
-            for (int j = 0; j < 1; j++) {
-                System.out.println("Nota: ");
-                matrice1[i][j] = input.nextInt();
-                System.out.println("Credite: ");
-                matrice2[i][j] = input.nextInt();
-                sum = matrice1[i][j] + matrice2[i][j];
-                if (matrice1[i][j] == 0 || matrice2[i][j] == 0)
-                    return;
-            }
-        }
-        int multiply = 1;
-        for (int i = 0; i < matrice2.length; i++)
-            for (int j = 0; j < matrice2.length; j++) {
-            multiply = matrice1[i][j] * matrice2[i][j];
-            }
+            System.out.println("---------");
 
-        int total = multiply / sum;
-        System.out.println("MEDIA ESTE: " + total);
+
+            System.out.print("Nota: ");
+            int rezMat1 = input.nextInt();
+            if (rezMat1 == 0) {
+                stopLoop = true;
+                break;
+            }
+            matrice1.add(rezMat1);
+
+            System.out.print("Credite: ");
+            int rezMat2 = input.nextInt();
+            matrice2.add(rezMat2);
+            System.out.println();
+
+            sum += matrice2.get(i);
+            i++;
+        }
+
+
+        double multiply = 0;
+        for (i = 0; i < matrice1.size(); i++) {
+            multiply += (matrice1.get(i) * matrice2.get(i));
+        }
+
+        double total = multiply / sum;
+        System.out.printf("MEDIA ESTE: %.2f", total);
     }
 
-
 }
-
